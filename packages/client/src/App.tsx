f7300d341b1a4383bd6f9bbd185937cdbedfe7b2
Wellerson <wllrsn.c@gmail.com>
2022-11-24 12:42:12 -0300

    feat: first frontend config with vitejs

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,14 @@
+
+import React, { Suspense } from "react";
+
+function App() {
+
+    return (
+        <Suspense fallback={"Loading..."}>
+            here
+            {/* <Router /> */}
+        </Suspense>
+    );
+}
+
+export default App;
