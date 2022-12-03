04ae53bbd37a6e9349a06bbbff434509fda93960
Wellerson <wllrsn.c@gmail.com>
2022-12-03 11:30:43 -0300

    feat: config

--- packages/server/src/configs/postgres/staging.ts
+++ packages/server/src/configs/postgres/staging.ts
@@ -1,11 +1,9 @@
 import { IKeysProps } from './'
 
 export const staging = {
-  DATABASE_RDS_URL: 'postgresql://postgres:postgres@localhost:4321/uploaddb',
+  DATABASE_RDS_URL: 'postgresql://postgres:postgres@localhost:5432/uploaddb',
   options: {
     dialect: 'postgres',
-    port: 4321,
-    host: "localhost",
     pool: {
       max: 100,
       min: 0,
