8c799a472240b4ec34ea5d5df72098164f16d418
Wellerson <wllrsn.c@gmail.com>
2022-12-03 00:31:00 -0300

    feat: created postgres from docker compose

--- packages/server/src/configs/postgres/staging.ts
+++ packages/server/src/configs/postgres/staging.ts
@@ -1,13 +1,16 @@
 import { IKeysProps } from './'
 
 export const staging = {
-  DATABASE_RDS_URL: 'postgres://postgres:postgres@localhost:5431/uploaddb',
+  DATABASE_RDS_URL: 'postgresql://postgres:postgres@localhost:4321/uploaddb',
   options: {
     dialect: 'postgres',
+    port: 4321,
+    host: "localhost",
     pool: {
       max: 100,
-      min: 0
+      min: 0,
+      acquire: 10000,
+      idle: 20000,
     },
-    logging: false
   }
 } as IKeysProps
