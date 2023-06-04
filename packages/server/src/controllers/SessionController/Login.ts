0697d29d4e396c443c7eabcb207900fd8f41ca05
Wellerson <wllrsn.c@gmail.com>
2023-06-04 09:01:47 -0300

    feat: add zod validation on register and login controller

--- packages/server/src/controllers/SessionController/Login.ts
+++ packages/server/src/controllers/SessionController/Login.ts
@@ -1,18 +1,38 @@
 import { Request, Response } from 'express'
-import LoginServices from "../../services/SessionServices/Login"
-
-
+import LoginServices from '../../services/SessionServices/Login'
+import z, { ZodError } from 'zod'
 
 const Login = async (request: Request, response: Response) => {
-    const { email, password } = request.body
+  try {
+    const createUserSchema = z.object({
+      email: z
+        .string({
+          required_error: 'Email é obrigatório'
+        })
+        .email('Email inválido'),
+      password: z.string({
+        required_error: 'Senha é obrigatório'
+      })
+    })
+
+    const { email, password } = createUserSchema.parse(request.body)
 
     if (!email || !password) {
-        return response.status(400).json({ message: "Email e senha são obrigatórios" })
+      return response
+        .status(400)
+        .json({ message: 'Email e senha são obrigatórios' })
     }
 
     const { message, token, status } = await LoginServices({ email, password })
 
     return response.status(status).json({ message, token })
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return response
+        .status(400)
+        .json({ message: error.errors[0].message, token: null })
+    }
+  }
 }
 
 export default Login
--- packages/server/src/controllers/SessionController/Register.ts
+++ packages/server/src/controllers/SessionController/Register.ts
@@ -1,16 +1,44 @@
 import { Request, Response } from 'express'
-import RegisterServices from "../../services/SessionServices/Register"
+import RegisterServices from '../../services/SessionServices/Register'
+import z, { ZodError } from 'zod'
 
 const Register = async (request: Request, response: Response) => {
-  const { name, email, password } = request.body
+  try {
+    const createUserSchema = z.object({
+      name: z.string({
+        required_error: 'Nome é obrigatório'
+      }),
+      email: z
+        .string({
+          required_error: 'Email é obrigatório'
+        })
+        .email('Email inválido'),
+      password: z.string({
+        required_error: 'Senha é obrigatório'
+      })
+    })
 
-  if (!name || !email || !password) {
-    return response.status(400).json({ token: null, message: "Nome, email e senha são obrigatórios" })
+    const { name, email, password } = createUserSchema.parse(request.body)
+
+    if (!name || !email || !password) {
+      return response
+        .status(400)
+        .json({ token: null, message: 'Nome, email e senha são obrigatórios' })
+    }
+
+    const { token, message, status } = await RegisterServices({
+      name,
+      email,
+      password
+    })
+
+    return response.status(status).json({ message, token })
+  } catch (error) {
+    if (error instanceof ZodError)
+      return response
+        .status(400)
+        .json({ message: error.errors[0].message, token: null })
   }
-
-  const { token, message, status } = await RegisterServices({ name, email, password })
-
-  return response.status(status).json({  message, token })
 }
 
 export default Register
